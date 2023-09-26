85f7761c908eb6f1cdade498af2b769165025a4a
david-wolff <wolff6897@gmail.com>
2023-09-26 13:35:28 -0300

    With db migrations + Driver model

--- MeuApp/models.py
+++ MeuApp/models.py
@@ -4,6 +4,21 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Driver(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, help_text='Enter the driver name')
+    age = models.IntegerField(help_text='Enter the age')
+    email = models.EmailField(help_text='Enter the email', max_length=254)
+    phone = models.CharField(help_text='Phone with country and area code', max_length=20)
+    vehicle_model = models.CharField(max_length=100, help_text='Enter the vehicle model')
+    vehicle_number = models.CharField(max_length=20, help_text='Enter the vehicle number')
+    available_seats = models.IntegerField(help_text='Enter the number of available seats')
+    destination = models.CharField(max_length=100, help_text='Enter the destination')
+    departure_time = models.DateTimeField(help_text='Enter the departure time')
+    is_available = models.BooleanField(default=True, help_text='Is the driver available for a ride?')
+
+    def __str__(self):
+        return self.name
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # additional fields can be added here
