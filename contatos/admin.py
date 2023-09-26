8d98e2dec8144048da113de621a8bdf105a05932
david-wolff <wolff6897@gmail.com>
2023-09-26 11:58:28 -0300

    Model implementation + new home page + css config

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from contatos.models import Pessoa
+from MeuApp.models import Ride  # Make sure MeuApp is the correct app name
 
 admin.site.register(Pessoa)
+admin.site.register(Ride)
