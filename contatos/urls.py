14a6968ec243d9b9d4afa33e884994e87ad37482
Antonio Mesquita Junior <antonio.mesquita@ifood.com.br>
2023-09-29 20:43:04 -0300

    feat: create contact view, form and model

--- contatos/urls.py
+++ contatos/urls.py
@@ -5,5 +5,6 @@ app_name = 'contatos'
 
 urlpatterns = [
     path('lista/', views.ContatoListView.as_view(), name='lista-contatos'),
-    path('', views.ContatoListView.as_view(), name='home-contatos')
+    path('', views.ContatoListView.as_view(), name='home-contatos'),
+    path('cria/', views.ContatoCreateView.as_view(), name='cria-contato')
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from contatos.forms import ContatoModel2Form
 from contatos.models import Pessoa
 from django.views.generic.base import View
 
@@ -9,3 +13,16 @@ class ContatoListView(View):
         pessoas = Pessoa.objects.all()
         context = {'pessoas': pessoas, }
         return render(request, 'contatos/listaContatos.html', context)
+
+
+class ContatoCreateView(View):
+    def get(self, request, *args, **kargs):
+        context = {'formulario': ContatoModel2Form}
+        return render(request, 'contatos/criaContato.html', context)
+
+    def post(self, request, *args, **kwargs):
+        formulario = ContatoModel2Form(request.POST)
+        if formulario.is_valid():
+            contato = formulario.save()
+            contato.save()
+            return HttpResponseRedirect(reverse_lazy('contatos:lista-contatos'))
